e05135353bb767d97beb84ddebfa980f636e157a
siiaaa <121426629+siiaaa@users.noreply.github.com>
2022-12-27 10:54:46 +0530

    Update footer.jsx
    Co-authored-by: Ajinkgupta <76146259+Ajinkgupta@users.noreply.github.com>

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -2,13 +2,11 @@ import React from "react";
   
 const Footer = () => {
   return (
-    <>
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
   <div>
-  <p>Copyright @ CoolCommunityX</p>
+  <p>Copyright @CoolCommunityX</p>
   </div>
 </footer>
-     </>
   );
 };
   
